4a8647952ea8ca53ccdc90f77c580663bf252fe6
alastair <alastair@alastairs-place.net>
2010-02-03 11:50:20 +0000

    Added keyboard commands for editing and opening.  Changed base class to NSControl to get target/action in IB, added a test target.  Also fixed some slightly odd behaviour with keyboard selections.

---
+++ TestTarget.h
@@ -0,0 +1,18 @@
+//
+//  TestTarget.h
+//  CSIconView
+//
+//  Created by Alastair Houghton on 03/02/2010.
+//  Copyright 2010 Coriolis Systems Limited. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface TestTarget : NSObject {
+
+}
+
+- (IBAction)itemClicked:(id)sender;
+
+@end
